ef61b1cc24ade69afdb38a0d8ffb324b259ea21a
Antonio Rodriguez <42835728+rodzanto@users.noreply.github.com>
2020-09-17 13:36:32 +0200

    Update create_eks_cluster.sh

--- create_eks_cluster.sh
+++ create_eks_cluster.sh
@@ -1,11 +1,11 @@
 eksctl create cluster \
     --name kubeflow-sm \
-    --version 1.15 \
-    --region us-west-2 \
+    --version 1.17 \
+    --region eu-west-1 \
     --nodegroup-name cpu-nodes \
     --node-type c5.xlarge \
     --nodes 2 \
     --node-volume-size 50 \
-    --node-zones us-west-2a \
+    --node-zones eu-west-1a \
     --timeout=40m \
     --auto-kubeconfig
